52b5dd97876a9580fa387b55d21696dec80bba24
MateuszK <mateusz.konopka11@gmail.com>
2020-02-06 23:35:00 +0100

    Now its time for registration and little UI Design.

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -29,7 +29,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ClientApp/src/app/login/login.component.ts
+++ ClientApp/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AccountService } from '../services/account.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,54 @@ import { AccountService } from '../services/account.service';
 })
 export class LoginComponent implements OnInit {
 
-  private getLoginStatus: boolean;
+  insertForm: FormGroup;
+  userName: FormControl;
+  password: FormControl;
+  returnUrl: string;
+  errorMessage: string;
+  invalidLogin: boolean;
 
-  constructor(private account: AccountService) { }
+  constructor(
+    private account: AccountService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
 
-    //this.getLoginStatus = this.account.
+    this.userName = new FormControl('', [Validators.required]);
+    this.password = new FormControl('', [Validators.required, Validators.minLength(4)]);
+
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+    this.insertForm = this.formBuilder.group({
+      "Username": this.userName,
+      "Password": this.password
+    });
+  }
+
+  onSubmit() {
+
+    let userLogin = this.insertForm.value;
+
+    this.account.login(userLogin.Username, userLogin.Password).subscribe(
+      result =>
+      {
+      let token = (<any>result).token;
+      console.log(token);  //show token in console <--
+      console.log(result.userRole);
+      console.log("Usser logged in successfully");
+      this.invalidLogin = false;
+      console.log(this.returnUrl);
+      this.router.navigateByUrl(this.returnUrl);
+      },
+      error =>
+      {
+        this.invalidLogin = true;
+        this.errorMessage = "Invalid data supplied";
+
+        console.log(this.errorMessage);
+      });
 
   }
 
